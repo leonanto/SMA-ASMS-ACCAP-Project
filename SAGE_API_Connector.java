5ab485c226ef8d6c09aac3a5fa6ddeffa826fee7
ASMS-ACCPAC-INT2 <ASMS-ACCPAC-2@SMA.com>
2016-02-23 10:42:47 -0500

    added classes to get info from ACCPAC tables

--- SAGE_API_Connector.java
+++ SAGE_API_Connector.java
@@ -17,13 +17,50 @@ public class SAGE_API_Connector {
 		     "admin", "SANLTD", new Date());
 		
 		Program program;
-		program = new Program(session, "XZ", "XZ0001", "10A");	
+		program = new Program(session, "XZ", "XZ0001", "01A");	
+		//System.out.print(getCustomers(program, "IDCUST"));
+		System.out.print(getInvoices(program));
+	}
 	
-		View customer = new View(program, "AR0024");
+	//Customers Table 
+	public static boolean getCustomers(Program program1, String field)
+	{ 
+		View customer = new View(program1, "AR0024");
 		customer.filterSelect("", true, 1, View.FilterOrigin.FromStart);
 		while (customer.goNext()) {
-			System.out.println(customer.getField("NAMECUST") + "\n");
+			System.out.println(customer.get(field).toString() + "\n");
 		}
+		return true;
+	}
+	
+	//Invoices Table
+	public static boolean getInvoices(Program program){
+		View invoices = new View(program, "AR0032");
+		invoices.filterSelect("", true, 1, View.FilterOrigin.FromStart);
+		while (invoices.goNext()) {
+			System.out.println(invoices.get("IDTRX").toString() + "\n");
+		}	
+		return true;
+	}
+	
+	//Invoice Details Table
+	public static boolean getInvoiceDetails(Program program){
+		View invoiceD = new View(program, "AR0033");
+		invoiceD.filterSelect("", true, 1, View.FilterOrigin.FromStart);
+		while (invoiceD.goNext()) {
+			System.out.println(invoiceD.get("IDTRX").toString() + "\n");
+		}	
+		return true;
+	}
+	
+	//Invoice Batches Table
+	public static boolean getInvoiceBatch(Program program){
+		View invoiceB = new View(program, "AR0031");
+		invoiceB.filterSelect("", true, 1, View.FilterOrigin.FromStart);
+		while (invoiceB.goNext()) {
+			System.out.println(invoiceB.get("IDTRX").toString() + "\n");
+		}	
+		return true;
 	}
 }
 
